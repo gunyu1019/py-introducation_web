eedd740a102b083ae9c7b368fb80d80446f9be63
gunyu1019 <gunyu1019@gmail.com>
2025-10-07 02:16:35 +0900

    [Feat] Implement introduction.py

---
+++ section/introduction.py
@@ -0,0 +1,14 @@
+from puepy import Application, Page, Component, t
+
+
+@t.component()
+class Introduction(Component):
+    props = []
+
+    def populate(self):
+        with t.div(classes=["about-section"]):
+            t.h3("👋 안녕하세요?")
+            t.span("저는 새로운 지식을 자유롭게 받아들이고, ")
+            t.span("세상에 도움이 되는 프로그램을 만드려고 노력하는 ")
+            t.span("소프트웨어 개발자 이용현 입니다!")
+        t.img()  # 배경 이미지
