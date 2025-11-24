5b018db4857e61405deadea510fb083c521cd70f
gunyu1019 <gunyu1019@gmail.com>
2025-11-25 00:17:35 +0900

    [Feat] Setup initial state in project

--- components/project.py
+++ components/project.py
@@ -1,3 +1,4 @@
+import json
 from puepy import Component, t
 
 
@@ -5,6 +6,16 @@ from puepy import Component, t
 class Project(Component):
     props = []
 
+    def initial(self):
+        f = open("project.json", mode="r", encoding='utf-8')
+        project_info = json.load(f)
+        f.close()
+        return {
+            "current_page": 0,
+            "max_page": len(project_info),
+            "data": project_info
+        }
+
     def populate(self):
         with t.div(classes=["project"]):
-            pass
+            t.span("진행했던 여러 개인 프로젝트를 소개합니다!", classes=["project-title"])
