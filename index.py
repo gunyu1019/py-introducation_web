b9a7f1db834e4fc7778c1b1104a0159cfe6a7e3b
gunyu1019 <gunyu1019@gmail.com>
2025-11-25 01:06:51 +0900

    [Feat] Apply pageable JS-module in main page.

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
-from puepy import Application, Page, Component, t
+from puepy import Application, Page, t
+from puepy.runtime import is_server_side
+from puepy.util import jsobj
 
 # noinspection PyUnresolvedReferences
 import introduction
@@ -7,17 +9,33 @@ import virtual_business_card
 # noinspection PyUnresolvedReferences
 import project
 
+if not is_server_side:
+    import js
+
 application = Application()
 
 
 @application.page()
 class MainPage(Page):
     def populate(self):
-        with t.div():
-            t.introduction()
-            t.project()
-            t.virtual_business_card()
+        with t.div(id="container"):
+            t.introduction(data_anchor="introduction")
+            t.project(data_anchor="project")
+            t.virtual_business_card(data_anchor="virtual_business_card")
         return
+
+    def on_ready(self):
+        if is_server_side:
+            return
+        pageable = js.Pageable.new("#container")
+        pageable.on("scroll", self.on_scroll)
+
+    def on_scroll(self, data):
+        # TOO-HARD CODING!!
+        if data["index"] != 0:
+            self.document.querySelector("div.introduction > img").style.display = "none"
+        else:
+            self.document.querySelector("div.introduction > img").style.display = "block"
 
 
 application.mount("#app")
